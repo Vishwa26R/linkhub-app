7ffeee3bedb5dbc4d74a0ccad70cb6a94f727ead
Vishwa Rajapaksha <vishwarajapaksha16@gmail.com>
2025-10-20 12:57:12 +0530

    test mongoDB connetion

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  // Add this line to debug
+  console.log('Connection String:', process.env.MONGO_URI); 
+
   try {
-    // Attempt to connect to the MongoDB database using the URI from our .env file
     const conn = await mongoose.connect(process.env.MONGO_URI);
     
-    // If connection is successful, log it to the console
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    // If there's an error, log the error message and exit the process
     console.error(`Error: ${error.message}`);
-    process.exit(1); // Exit with a failure code
+    process.exit(1);
   }
 };
 
